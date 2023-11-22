b9511ac3507e0d799e2961ea04d19c605126bfb6
dado <dado.zh@gmail.com>
2023-11-22 15:33:47 +0100

    removed download section

--- src/installer.ts
+++ src/installer.ts
@@ -21,38 +21,26 @@ if (!tempDirectory) {
   tempDirectory = path.join(baseLocation, 'actions', 'temp');
 }
 
-export async function getMaven(version: string, localPath?: string, downloadUrl?: string) {
+export async function getMaven(version: string, localPath?: string) {
   let toolPath: string;
   toolPath = tc.find('maven', version);
 
   if (!toolPath) {
-    toolPath = await downloadMaven(version, downloadUrl, localPath);
+    toolPath = await extractMaven(version, localPath);
   }
 
   toolPath = path.join(toolPath, 'bin');
   core.addPath(toolPath);
 }
 
-async function downloadMaven(version: string, localPath?: string, downloadUrl?: string ): Promise<string> {
-  if (localPath) {
+async function extractMaven(version: string, localPath?: string): Promise<string> {
+  if (!localPath) {
+    core.error("localPath was not provided: " + localPath);
+  }
+
     // Use the locally stored file
-    const extractedPath = await tc.extractTar(localPath);
+    const extractedPath = await tc.extractTar(localPath!);
     let toolRoot = path.join(extractedPath, `apache-maven-${version}`);
     return await tc.cacheDir(toolRoot, 'maven', version);
-  }
 
-  // If localPath is not provided, download from the specified URL
-  const toolDirectoryName = `apache-maven-${version}`;
-  const finalDownloadUrl = downloadUrl || `https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/${version}/apache-maven-${version}-bin.tar.gz`;
-
-  console.log(`downloading ${finalDownloadUrl}`);
-
-  try {
-    const downloadPath = await tc.downloadTool(finalDownloadUrl);
-    const extractedPath = await tc.extractTar(downloadPath);
-    let toolRoot = path.join(extractedPath, toolDirectoryName);
-    return await tc.cacheDir(toolRoot, 'maven', version);
-  } catch (err) {
-    throw err;
-  }
 }
