0d1095ebc3c4a9c5e02c1393570d423651b63906
dado <dado.zh@gmail.com>
2023-11-22 14:47:20 +0100

    added option to work with locally available maven

--- src/installer.ts
+++ src/installer.ts
@@ -1,9 +1,11 @@
-// Load tempDirectory before it gets wiped by tool-cache
-let tempDirectory = process.env['RUNNER_TEMPDIRECTORY'] || '';
+
 
 import * as core from '@actions/core';
 import * as tc from '@actions/tool-cache';
 import * as path from 'path';
+
+// Load tempDirectory before it gets wiped by tool-cache
+let tempDirectory = process.env['RUNNER_TEMPDIRECTORY'] || '';
 
 if (!tempDirectory) {
   let baseLocation: string;
@@ -19,25 +21,34 @@ if (!tempDirectory) {
   tempDirectory = path.join(baseLocation, 'actions', 'temp');
 }
 
-export async function getMaven(version: string) {
+export async function getMaven(version: string, downloadUrl?: string, localPath?: string) {
   let toolPath: string;
   toolPath = tc.find('maven', version);
 
   if (!toolPath) {
-    toolPath = await downloadMaven(version);
+    toolPath = await downloadMaven(version, downloadUrl, localPath);
   }
 
   toolPath = path.join(toolPath, 'bin');
   core.addPath(toolPath);
 }
 
-async function downloadMaven(version: string): Promise<string> {
+async function downloadMaven(version: string, downloadUrl?: string, localPath?: string): Promise<string> {
+  if (localPath) {
+    // Use the locally stored file
+    const extractedPath = await tc.extractTar(localPath);
+    let toolRoot = path.join(extractedPath, `apache-maven-${version}`);
+    return await tc.cacheDir(toolRoot, 'maven', version);
+  }
+
+  // If localPath is not provided, download from the specified URL
   const toolDirectoryName = `apache-maven-${version}`;
-  const downloadUrl = `https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/${version}/apache-maven-${version}-bin.tar.gz`;
-  console.log(`downloading ${downloadUrl}`);
+  const finalDownloadUrl = downloadUrl || `https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/${version}/apache-maven-${version}-bin.tar.gz`;
+
+  console.log(`downloading ${finalDownloadUrl}`);
 
   try {
-    const downloadPath = await tc.downloadTool(downloadUrl);
+    const downloadPath = await tc.downloadTool(finalDownloadUrl);
     const extractedPath = await tc.extractTar(downloadPath);
     let toolRoot = path.join(extractedPath, toolDirectoryName);
     return await tc.cacheDir(toolRoot, 'maven', version);
